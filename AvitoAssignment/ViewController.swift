7fe57e36605257988d61431097d2c39ff4826723
poskreepta <poskreepta@yandex.ru>
2023-08-25 11:52:23 +0400

    Initial Commit

---
+++ AvitoAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoAssignment
+//
+//  Created by poskreepta on 25.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
